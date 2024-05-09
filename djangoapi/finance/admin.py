7df2e983367c23578227cdccf892d1225f6ef5ea
Oliver <olimovier@gmail.com>
2024-05-09 14:41:36 +0800

    Register tmall refund at admin panel

--- djangoapi/finance/admin.py
+++ djangoapi/finance/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import TmallRefund
 
-# Register your models here.
+@admin.register(TmallRefund)
+class TmallRefundAdmin(admin.ModelAdmin): 
+    list_display = [
+        "trade_no",
+        "refund_no",
+        "alipay_transaction_no",
+        "pay_time",
+        "goods_no",
+        "refund_close_time",
+        "paid",
+        "goods_name",
+        "refund",
+        "refund_source",
+        "goods_return",
+        "refund_apply_time",
+        "refund_deadline",
+        "refund_status",
+        "goods_status",
+        "return_logistics_info",
+        "deliver_logistics_info",
+        "service_intervene",
+        "seller_name",
+        "seller_name_new",
+        "seller_return_address",
+        "seller_zip",
+        "seller_telno",
+        "seller_mobile",
+        "logistics_no",
+        "logistics_name",
+        "refund_reason",
+        "refund_explanation",
+    ]
