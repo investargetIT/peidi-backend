d1250ca2fcb78fd44fcfa24acd9541b1b0993aeb
Oliver <olimovier@gmail.com>
2024-05-09 13:57:12 +0800

    New app finance and new model tmall refund

--- djangoapi/main/settings.py
+++ djangoapi/main/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework.authtoken',
+    'finance.apps.FinanceConfig',
 ]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
