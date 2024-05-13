ceb7884136242204568a67642e8389cb06d8b8fe
Oliver <olimovier@gmail.com>
2024-05-13 16:39:03 +0800

    Run scheduler in entrypoint script

--- djangoapi/entrypoint.sh
+++ djangoapi/entrypoint.sh
@@ -1,5 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 python manage.py makemigrations
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000 &
+python manage.py runapscheduler &
+wait -n
+exit $?
