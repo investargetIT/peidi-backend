36c15daf7a43f0c66a97419e2eabf8a097b322fc
Oliver <olimovier@gmail.com>
2024-05-14 08:32:54 +0800

    Update apscheduler start point

--- djangoapi/entrypoint.sh
+++ djangoapi/entrypoint.sh
@@ -2,7 +2,7 @@
 
 python manage.py makemigrations
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000 &
-python manage.py runapscheduler &
-wait -n
-exit $?
+python manage.py runserver 0.0.0.0:8000
+# python manage.py runapscheduler &
+# wait -n
+# exit $?
--- djangoapi/schedulers/views.py
+++ djangoapi/schedulers/views.py
@@ -1,41 +1,29 @@
 import json
 from django.http import JsonResponse
 from rest_framework import viewsets
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 from django_apscheduler.jobstores import DjangoJobStore
 from utils.customclass import SuccessResponse
-from django.conf import settings
-from schedulers.management.commands.runapscheduler import scheduler
 from apscheduler.triggers.cron import CronTrigger
+
+scheduler = BackgroundScheduler()
+scheduler.add_jobstore(DjangoJobStore(), "default")
+scheduler.start()
+
+# 具体要执行的代码
+def test():
+    f = open("/code/utils/demo.txt", "a")
+    f.write("Now the file has more content!\n")
+    f.close()
+    pass
 
 class DingTalkIMRobot(viewsets.ModelViewSet):
 
-
-
     # 与前端的接口
     def test_add_task(self, request, *args, **kwargs):
-
-        # 具体要执行的代码
-        def test():
-            f = open("/code/utils/demo.txt", "a")
-            f.write("Now the file has more content!\n")
-            f.close()
-            pass
-
-        # scheduler = BlockingScheduler(timezone=settings.TIME_ZONE)
-        # scheduler.add_jobstore(DjangoJobStore(), "default")
-        # content = json.loads(request.body.decode())  # 接收参数
         try:
-            # start_time = content['start_time']  # 用户输入的任务开始时间, '10:00:00'
-            # start_time = start_time.split(':')
-            # hour = int(start_time)[0]
-            # minute = int(start_time)[1]
-            # second = int(start_time)[2]
-            # s = content['s']  # 接收执行任务的各种参数
-            
             # 创建任务
             # scheduler.add_job(test, 'cron', hour=hour, minute=minute, second=second, args=[s])
-            print(scheduler)
             scheduler.add_job(
                 test,
                 trigger=CronTrigger(second="*/10"),  # Every 10 seconds
