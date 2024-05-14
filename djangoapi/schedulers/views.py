4a4f85124d3e6cfba9c3e716eeed96d5d8deab0c
Oliver <olimovier@gmail.com>
2024-05-14 09:29:55 +0800

    Integrate apscheduler with endpoint

--- djangoapi/schedulers/views.py
+++ djangoapi/schedulers/views.py
@@ -1,10 +1,13 @@
 import json
+import logging
+from datetime import datetime
 from django.http import JsonResponse
 from rest_framework import viewsets
 from apscheduler.schedulers.background import BackgroundScheduler
 from django_apscheduler.jobstores import DjangoJobStore
 from utils.customclass import SuccessResponse
 from apscheduler.triggers.cron import CronTrigger
+from apscheduler.triggers.date import DateTrigger
 
 scheduler = BackgroundScheduler()
 scheduler.add_jobstore(DjangoJobStore(), "default")
@@ -17,17 +20,30 @@ def test():
     f.close()
     pass
 
+def test1(content):
+    f = open("/code/utils/demo1.txt", "w")
+    f.write(content)
+    f.close()
+    pass
+
 class DingTalkIMRobot(viewsets.ModelViewSet):
 
     # 与前端的接口
     def test_add_task(self, request, *args, **kwargs):
         try:
             # 创建任务
-            # scheduler.add_job(test, 'cron', hour=hour, minute=minute, second=second, args=[s])
+            # scheduler.add_job(
+            #     test,
+            #     trigger=CronTrigger(second="*/10"),  # Every 10 seconds
+            #     id="my_job1",  # The `id` assigned to each job MUST be unique
+            #     max_instances=1,
+            #     replace_existing=True,
+            # )
             scheduler.add_job(
-                test,
-                trigger=CronTrigger(second="*/10"),  # Every 10 seconds
-                id="my_job1",  # The `id` assigned to each job MUST be unique
+                test1,
+                trigger=DateTrigger('2024-05-14 09:06:00'),
+                id='job_id1',
+                args=['date_job3'],
                 max_instances=1,
                 replace_existing=True,
             )
