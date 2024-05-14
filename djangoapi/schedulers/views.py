609fb9d3665fb675db49f8196575a52dea7d826d
Oliver <olimovier@gmail.com>
2024-05-14 13:00:22 +0800

    Prepare endpoint for schedule a dingtalk msg send

--- djangoapi/schedulers/views.py
+++ djangoapi/schedulers/views.py
@@ -1,11 +1,9 @@
-import json
-import logging
-from datetime import datetime
-from django.http import JsonResponse
+import os, json, requests
+
 from rest_framework import viewsets
 from apscheduler.schedulers.background import BackgroundScheduler
 from django_apscheduler.jobstores import DjangoJobStore
-from utils.customclass import SuccessResponse
+from utils.customclass import SuccessResponse, PeiDiError, ExceptionResponse, PeiDiErrorResponse
 from apscheduler.triggers.cron import CronTrigger
 from apscheduler.triggers.date import DateTrigger
 
@@ -32,49 +30,55 @@ def test2(content):
     f.close()
     pass
 
+def send_dingtalk_msg(content, mobiles):
+    url = os.environ.get('DINGTALK_IMROBOT_WEBHOOK')
+    data = {
+        "msgtype": "text",
+        "text": {
+            "content": content
+        },
+        "at": {
+	        "atMobiles": mobiles.split(',')
+        }
+    }
+    response = requests.post(url, data=data).content
+    res = json.loads(response.decode())
+    print(res)
+    errcode = res.get('errcode')
+    if errcode != 0:
+        raise PeiDiError(20501, msg=res['errmsg'])
+    return res['result']['userid']
+
 class DingTalkIMRobot(viewsets.ModelViewSet):
 
     # 与前端的接口
-    def test_add_task(self, request, *args, **kwargs):
-        try:
-            # 创建任务
-            # scheduler.add_job(
-            #     test,
-            #     trigger=CronTrigger(second="*/10"),  # Every 10 seconds
-            #     id="my_job1",  # The `id` assigned to each job MUST be unique
-            #     max_instances=1,
-            #     replace_existing=True,
-            # )
+    def test_add_task(self, request):
+        content = request.data.get('content')
+        at_mobiles = request.data.get('at_mobiles')
 
-            # scheduler.add_job(
-            #     test1,
-            #     trigger=DateTrigger('2024-05-14 09:06:00'),
-            #     id='job_id1',
-            #     args=['date_job3'],
-            #     max_instances=1,
-            #     replace_existing=True,
-            # )
+        # 创建任务
+        # scheduler.add_job(
+        #     test,
+        #     trigger=CronTrigger(second="*/10"),  # Every 10 seconds
+        #     id="my_job1",  # The `id` assigned to each job MUST be unique
+        #     max_instances=1,
+        #     replace_existing=True,
+        # )
 
-            scheduler.add_job(
-                test2,
-                id='job_id2',
-                args=['date_job2'],
-                max_instances=1,
-                replace_existing=True,
-            )
-            
-            code = '200'
-            message = 'success'
-        except Exception as e:
-            print(e)
-            code = '400'
-            message = e
-            
-        back = {
-            'code': code,
-            'message': message
-        }
-        # return JsonResponse(json.dumps(back, ensure_ascii=False), safe=False)
-        return SuccessResponse({'count': 0, 'data': []})
-        
+        # scheduler.add_job(
+        #     test1,
+        #     trigger=DateTrigger('2024-05-14 09:06:00'),
+        #     id='job_id1',
+        #     args=['date_job3'],
+        #     max_instances=1,
+        #     replace_existing=True,
+        # )
 
+        scheduler.add_job(
+            send_dingtalk_msg,
+            args=[content, at_mobiles],
+            max_instances=1,
+            replace_existing=True,
+        )
+
+        return SuccessResponse('定时任务创建成功')
