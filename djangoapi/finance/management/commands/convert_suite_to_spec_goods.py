298c22b2e389213b6a2f1c6136d71c80280f25a1
Oliver <olimovier@gmail.com>
2024-05-23 15:23:47 +0800

    Update custom command

--- djangoapi/finance/management/commands/convert_suite_to_spec_goods.py
+++ djangoapi/finance/management/commands/convert_suite_to_spec_goods.py
@@ -9,21 +9,23 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # all_invoices = Invoice.objects.all()
         # print(len(all_invoices))
-        # for invoice in all_invoices:
-        #     if invoice.goods_model:
-        #         self.goods_model_to_spec_goods(invoice.goods_model)
-        self.merge_original_invoice('2102048365776056051')
+
+        merged_invoice = self.merge_original_invoice('2102048365776056051')
+        for invoice in merged_invoice:
+            self.goods_model_to_spec_goods(invoice.goods_no)
     
     def goods_model_to_spec_goods(self, goods_model):
         try:
             spec_goods = SpecGoods.objects.get(spec_no=goods_model)
-            print('单品', spec_goods.spec_no, spec_goods.goods_name)
+            print(spec_goods.spec_no, '是单品', spec_goods.goods_name)
+            print()
         except SpecGoods.DoesNotExist:
             suite_goods = SuiteGoodsRec.objects.filter(suite_no=goods_model)
             if len(suite_goods) > 0:
-                print('组合装，包括以下单品：')
+                print(goods_model, '是组合装，包括以下单品：')
                 for goods in suite_goods:
-                    print(goods.spec_no, goods.goods_name)
+                    print(goods.spec_no, goods.goods_name, goods.num, goods.ratio)
+                print()
             else:
                 print('该商品不存在', goods_model)
                 # 作为单品处理
@@ -48,5 +50,5 @@ class Command(BaseCommand):
             num = target_item[0]['goods_num']
             print(invoice_time, shop_name, goods_no, num, price_with_tax)
             result.append(FinanceSalesAndInvoice(invoice_time=invoice_time, shop_name=shop_name, goods_no=goods_no, num=num, price_with_tax=price_with_tax))
-
+        print()
         return result
