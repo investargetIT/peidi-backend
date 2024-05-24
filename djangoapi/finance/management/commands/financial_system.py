f63b504dcc44ba44a469d9aa44d0f22946105dba
Oliver <olimovier@gmail.com>
2024-05-24 16:44:17 +0800

    Fix financial system custom command

--- djangoapi/finance/management/commands/financial_system.py
+++ djangoapi/finance/management/commands/financial_system.py
@@ -17,15 +17,17 @@ class Command(BaseCommand):
         #         self.goods_model_to_spec_goods(invoice)
 
         # self.finance_sales_invoice_summary('2024-04-01', '2024-04-25')
+
         # self.goods_sales_summary("2024-03-26", "2024-04-25")
-        self.extend_douyin_refund("2024020217216926772842869363881")
+
+        self.extend_douyin_refund("2024012614376926537769282508767")
     
     def goods_model_to_spec_goods(self, finance_sales_and_invoice):
         goods_model = finance_sales_and_invoice.goods_no
         try:
             spec_goods = SpecGoods.objects.get(spec_no=goods_model)
             # print(spec_goods.spec_no, '是单品', spec_goods.goods_name)
-            finance_sales_and_invoice.u9_no = self.goods_no_to_material_no(spec_goods.spec_no)
+            finance_sales_and_invoice.material_no = self.goods_no_to_material_no(spec_goods.spec_no)
             finance_sales_and_invoice.goods_name = spec_goods.goods_name
             finance_sales_and_invoice.save()
             # print()
@@ -35,8 +37,16 @@ class Command(BaseCommand):
                 # print(goods_model, '是组合装，包括以下单品：')
                 for goods in suite_goods:
                     # print(goods.spec_no, goods.goods_name, goods.num, goods.ratio)
-                    u9_no = self.goods_no_to_material_no(goods.spec_no)
-                    f = FinanceSalesAndInvoice(invoice_time=finance_sales_and_invoice.invoice_time, shop_name=finance_sales_and_invoice.shop_name, u9_no=u9_no, goods_no=goods.spec_no, goods_name=goods.goods_name, num=finance_sales_and_invoice.num*goods.num, price_with_tax=finance_sales_and_invoice.price_with_tax*goods.ratio)
+                    material_no = self.goods_no_to_material_no(goods.spec_no)
+                    f = FinanceSalesAndInvoice(
+                        date=finance_sales_and_invoice.date,
+                        shop_name=finance_sales_and_invoice.shop_name,
+                        material_no=material_no,
+                        goods_no=goods.spec_no,
+                        goods_name=goods.goods_name,
+                        invoice_num=finance_sales_and_invoice.invoice_num*goods.num,
+                        invoice_amount=finance_sales_and_invoice.invoice_amount*goods.ratio
+                    )
                     f.save()
                 # print()
             else:
@@ -49,16 +59,22 @@ class Command(BaseCommand):
         # 以订单id、商品型号、开票日期和店铺名称为唯一数据，合并发票总金额，即对冲掉优惠返现等负的发票总金额
         invoices = Invoice.objects.values("trade_no", "goods_model", "invoice_time", "shop_name").filter(trade_no=trade_no).annotate(Sum("goods_total_amount"), Sum("goods_num"))
         for invoice in invoices:
-            invoice_time = invoice['invoice_time']
+            date = invoice['invoice_time']
             shop_name = invoice['shop_name']
             goods_no = invoice['goods_model']
-            num = invoice['goods_num__sum']
-            price_with_tax = invoice['goods_total_amount__sum']
+            invoice_num = invoice['goods_num__sum']
+            invoice_amount = invoice['goods_total_amount__sum']
+            print(date, shop_name, goods_no, invoice_num, invoice_amount)
 
-            # print(invoice_time, shop_name, goods_no, num, price_with_tax)
-            f = FinanceSalesAndInvoice(invoice_time=invoice_time, shop_name=shop_name, goods_no=goods_no, num=num, price_with_tax=price_with_tax)
+            f = FinanceSalesAndInvoice(
+                date=date,
+                shop_name=shop_name,
+                goods_no=goods_no,
+                invoice_num=invoice_num,
+                invoice_amount=invoice_amount
+            )
             result.append(f)
-        # print()
+        print()
         return result
 
     def goods_no_to_material_no(self, goods_no):
@@ -131,5 +147,7 @@ class Command(BaseCommand):
         print(trade_no)
 
         salesout_records = salesOutDetails.objects.filter(otid=trade_no)
+        overall_amount = salesout_records.aggregate(Sum("deal_total_price"))
+        print('overall amount', overall_amount)
         for i in salesout_records:
             print(i)
