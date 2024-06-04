1d64d80f5a7cd7df2b4630b3c5616433c386c20f
Oliver <olimovier@gmail.com>
2024-06-04 14:46:48 +0800

    调用存储过程 GetSalesAmountRanking 导出到 APITable 渠道销量

--- djangoapi/orders/management/commands/get_shop_sales_amount.py
+++ djangoapi/orders/management/commands/get_shop_sales_amount.py
@@ -1,12 +1,40 @@
+import os, time, requests
+
 from django.core.management.base import BaseCommand
 from django.db import connection
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        url = os.getenv("APITABLE_BASE_URL") + "/fusion/v1/datasheets/dstRtoaAaN07lFArUX/records"
+        token = os.getenv("APITABLE_TOKEN")
+        records = []
+
         with connection.cursor() as cursor:
-            pay_time_start = '2022-01-01 00:00:00'
-            pay_time_end = '2022-12-31 23:59:59'
-            cursor.callproc('GetShopSalesAmount', (pay_time_start, pay_time_end))
+            time_start = '2024-01-01 00:00:00'
+            time_end = '2024-01-31 23:59:59'
+            cursor.callproc('GetSalesAmountRanking', (time_start, time_end))
             rows = cursor.fetchall()
             for row in rows:
                 print(row)
+                r = {
+                    "渠道": row[0],
+                    "店铺": row[1],
+                    "产品": row[2],
+                    "销售额": float(row[3]),
+                    "时间": time_start[:7]
+                }
+                records.append({ "fields": r })
+
+        print(len(records))
+        for i in range(int(len(records)/30)+1):
+            s = 30 * i
+            e = 30 * (i + 1)
+            if i == int(len(records)/30):
+                e = len(records)
+            res = requests.post(
+                url=url,
+                json={"records": records[s:e]},
+                headers={"Authorization": f"Bearer {token}"},
+            )
+            res.raise_for_status()
+            time.sleep(1)
