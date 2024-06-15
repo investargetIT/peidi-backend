c3d27d7d2f31043db81d9c63252e1576dfb269f7
Oliver <olimovier@gmail.com>
2024-06-15 16:33:33 +0800

    Comment out some endpoints

--- djangoapi/finance/urls.py
+++ djangoapi/finance/urls.py
@@ -1,21 +1,21 @@
 from django.urls import path
 from . import views
 
-# tmallRefundapi = views.TmallRefundView.as_view({
-#     'post': 'create',
-# })
-# pddRefundapi = views.PddRefundView.as_view({
-#     'post': 'create',
-# })
-# jdRefundapi = views.JdRefundView.as_view({
-#     'post': 'create',
-# })
-# douyinRefundapi = views.DouyinRefundView.as_view({
-#     'post': 'create',
-# })
-# invoiceapi = views.InvoiceView.as_view({
-#     'post': 'create',
-# })
+tmallRefundapi = views.TmallRefundView.as_view({
+    'post': 'create',
+})
+pddRefundapi = views.PddRefundView.as_view({
+    'post': 'create',
+})
+jdRefundapi = views.JdRefundView.as_view({
+    'post': 'create',
+})
+douyinRefundapi = views.DouyinRefundView.as_view({
+    'post': 'create',
+})
+invoiceapi = views.InvoiceView.as_view({
+    'post': 'create',
+})
 GoodsSalesSummaryAPI = views.GoodsSalesSummaryView.as_view({
     'post': 'create',
 })
@@ -27,12 +27,12 @@ PDMaterialNOListAPI = views.PDMaterialNOListView.as_view({
 })
 
 urlpatterns = [
-    # path("invoice", invoiceapi, name="invoiceapi"),
+    path("invoice", invoiceapi, name="invoiceapi"),
     path("goods_sales_summary", GoodsSalesSummaryAPI, name="GoodsSalesSummaryAPI"),
     path("sales_and_invoice", FinanceSalesAndInvoiceAPI, name="FinanceSalesAndInvoiceAPI"),
     path("pd_material_no", PDMaterialNOListAPI, name="PDMaterialNOListAPI"),
-    # path("refund/tmall", tmallRefundapi, name="tmallRefundapi"),
-    # path("refund/pdd", pddRefundapi, name="pddRefundapi"),
-    # path("refund/jd", jdRefundapi, name="jdRefundapi"),
-    # path("refund/douyin", douyinRefundapi, name="douyinRefundapi"),
+    path("refund/tmall", tmallRefundapi, name="tmallRefundapi"),
+    path("refund/pdd", pddRefundapi, name="pddRefundapi"),
+    path("refund/jd", jdRefundapi, name="jdRefundapi"),
+    path("refund/douyin", douyinRefundapi, name="douyinRefundapi"),
 ]
--- djangoapi/finance/views.py
+++ djangoapi/finance/views.py
@@ -79,41 +79,41 @@ class PddRefundView(viewsets.ModelViewSet):
         except Exception:
             return ExceptionResponse(traceback.format_exc().split('\n')[-2])
 
-# class JdRefundView(viewsets.ModelViewSet):
+class JdRefundView(viewsets.ModelViewSet):
 
-#     serializer_class = JdRefundSerializer
-#     authentication_classes = [TokenAuthentication]
-#     permission_classes = [IsAuthenticated]    
+    serializer_class = JdRefundSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]    
 
-#     def create(self, request, *args, **kwargs):
-#         try:
-#             datas = request.data
-#             if isinstance(datas, list):
-#                 success, fail = [], []
-#                 for data in datas:
-#                     try:
-#                         with transaction.atomic():
-#                             serializer = self.serializer_class(data=data)
-#                             if serializer.is_valid():
-#                                 serializer.save()
-#                                 success.append(serializer.data)
-#                             else:
-#                                 fail.append({'data': data, 'errmsg': serializer.errors})
-#                     except Exception as err:
-#                         fail.append({'data': data, 'errmsg': str(err)})
-#                 return SuccessResponse({'success': success, 'fail': fail})
-#             else:
-#                 with transaction.atomic():
-#                     serializer = self.serializer_class(data=datas)
-#                     if serializer.is_valid():
-#                         serializer.save()
-#                     else:
-#                         raise PeiDiError(20071, msg='新增京东仅退款失败', detail='%s' % serializer.errors)
-#                 return SuccessResponse(serializer.data)
-#         except PeiDiError as err:
-#             return PeiDiErrorResponse(err)
-#         except Exception:
-#             return ExceptionResponse(traceback.format_exc().split('\n')[-2])
+    def create(self, request, *args, **kwargs):
+        try:
+            datas = request.data
+            if isinstance(datas, list):
+                success, fail = [], []
+                for data in datas:
+                    try:
+                        with transaction.atomic():
+                            serializer = self.serializer_class(data=data)
+                            if serializer.is_valid():
+                                serializer.save()
+                                success.append(serializer.data)
+                            else:
+                                fail.append({'data': data, 'errmsg': serializer.errors})
+                    except Exception as err:
+                        fail.append({'data': data, 'errmsg': str(err)})
+                return SuccessResponse({'success': success, 'fail': fail})
+            else:
+                with transaction.atomic():
+                    serializer = self.serializer_class(data=datas)
+                    if serializer.is_valid():
+                        serializer.save()
+                    else:
+                        raise PeiDiError(20071, msg='新增京东仅退款失败', detail='%s' % serializer.errors)
+                return SuccessResponse(serializer.data)
+        except PeiDiError as err:
+            return PeiDiErrorResponse(err)
+        except Exception:
+            return ExceptionResponse(traceback.format_exc().split('\n')[-2])
 
 class DouyinRefundView(viewsets.ModelViewSet):
 
