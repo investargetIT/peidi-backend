8194870ba65ae2d2ee9664dce3f582abe9d8718a
Oliver <olimovier@gmail.com>
2024-06-19 13:07:15 +0800

    库存明细Excel导入

--- djangoapi/orders/admin.py
+++ djangoapi/orders/admin.py
@@ -2,9 +2,84 @@ from datetime import datetime
 
 from django.contrib import admin
 from django.utils.html import format_html
+
 from rangefilter.filters import DateTimeRangeFilterBuilder
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from import_export.fields import Field
 
 from .models import orders, salesOutDetails, historySalesOutDetails, OrderDetail, WMSShipData, StockDetail, ExchangeManagement, ShopTarget
+
+class StockDetailResource(resources.ModelResource):
+    spec_no = Field(attribute='spec_no', column_name='商家编码')
+    goods_no = Field(attribute='goods_no', column_name='货品编号')
+    goods_name = Field(attribute='goods_name', column_name='货品名称')
+    goods_short_name = Field(attribute='goods_short_name', column_name='货品简称')
+    goods_tag = Field(attribute='goods_tag', column_name='货品标签')
+    goods_type = Field(attribute='goods_type', column_name='分类')
+    spec_id = Field(attribute='spec_id', column_name='规格码')
+    spec_name = Field(attribute='spec_name', column_name='规格名称')
+    barcode = Field(attribute='barcode', column_name='条码')
+    weight = Field(attribute='weight', column_name='单品重量')
+    brand_name = Field(attribute='brand_name', column_name='品牌')
+    is_defective = Field(attribute='is_defective', column_name='残次品')
+    stock = Field(attribute='stock', column_name='库存')
+    unit_name = Field(attribute='unit_name', column_name='单位')
+    aux_unit_name = Field(attribute='aux_unit_name', column_name='辅助单位')
+    aux_remark = Field(attribute='aux_remark', column_name='辅助说明')
+    deliverable_stock = Field(attribute='deliverable_stock', column_name='可发库存')
+    min_alert_stock = Field(attribute='min_alert_stock', column_name='警戒库存下限')
+    max_alert_stock = Field(attribute='max_alert_stock', column_name='警戒库存上限')
+    unpaid_num = Field(attribute='unpaid_num', column_name='未付款量')
+    qa_num = Field(attribute='qa_num', column_name='已质检库存')
+    todeliver_order_num = Field(attribute='todeliver_order_num', column_name='未发货订单总数')
+    preorder_num = Field(attribute='preorder_num', column_name='预订单量')
+    toreview_num = Field(attribute='toreview_num', column_name='待审核量')
+    todeliver_num = Field(attribute='todeliver_num', column_name='待发货量')
+    lock_num = Field(attribute='lock_num', column_name='锁定量')
+    topurchase_num = Field(attribute='topurchase_num', column_name='待采购量')
+    purchase_ontheway_num = Field(attribute='purchase_ontheway_num', column_name='采购在途')
+    purchase_arrival_num = Field(attribute='purchase_arrival_num', column_name='采购到货量')
+    totransfer_num = Field(attribute='totransfer_num', column_name='待调拨量')
+    totransout_num = Field(attribute='totransout_num', column_name='待调出量')
+    transfer_ontheway_num = Field(attribute='transfer_ontheway_num', column_name='调拨在途')
+    toinstock_num = Field(attribute='toinstock_num', column_name='其他待入库量')
+    tooutstock_num = Field(attribute='tooutstock_num', column_name='其他待出库量')
+    toqa_num = Field(attribute='toqa_num', column_name='待质检量')
+    purchase_return_num = Field(attribute='purchase_return_num', column_name='采购退货')
+    sale_return_ontheway_num = Field(attribute='sale_return_ontheway_num', column_name='退货在途量')
+    produce_tooutstock_num = Field(attribute='produce_tooutstock_num', column_name='生产待出库量')
+    produce_toinstock_num = Field(attribute='produce_toinstock_num', column_name='生产待入库量')
+    material_no = Field(attribute='material_no', column_name='U9料号')
+    tax_rate = Field(attribute='tax_rate', column_name='税率')
+    remark = Field(attribute='remark', column_name='备注')
+    goods_remark = Field(attribute='goods_remark', column_name='货品备注')
+    specgoods_remark = Field(attribute='specgoods_remark', column_name='单品备注')
+    spec_created = Field(attribute='spec_created', column_name='单品创建时间')
+    last_inventory_time = Field(attribute='last_inventory_time', column_name='最后盘点时间')
+    usable_stock = Field(attribute='usable_stock', column_name='可用库存')
+    img_url = Field(attribute='img_url', column_name='图片链接')
+    img = Field(attribute='img', column_name='图片')
+    major_supplier = Field(attribute='major_supplier', column_name='主供应商')
+    custom_stock_one = Field(attribute='custom_stock_one', column_name='自定义库存1')
+    custom_stock_two = Field(attribute='custom_stock_two', column_name='自定义库存2')
+    custom_stock_three = Field(attribute='custom_stock_three', column_name='自定义库存3')
+    custom_stock_four = Field(attribute='custom_stock_four', column_name='自定义库存4')
+    custom_stock_five = Field(attribute='custom_stock_five', column_name='自定义库存5')
+    actual_stock_num = Field(attribute='actual_stock_num', column_name='实际库存')
+    actual_todelivery_stock_num = Field(attribute='actual_todelivery_stock_num', column_name='实际可发库存')
+
+    def before_import_row(self, row, **kwargs):
+        float_fields = ["单品创建时间", "最后盘点时间", "税率", "可用库存"]
+        for float_field in float_fields:
+            if not row[float_field]:
+                row[float_field] = None
+
+    def before_import(self, dataset, **kwargs):
+        StockDetail.objects.all().delete()
+
+    class Meta:
+        model = StockDetail
 
 @admin.register(orders)
 class OrderAdmin(admin.ModelAdmin): 
@@ -307,7 +382,7 @@ class HistorySalesOutDetailsAdmin(admin.ModelAdmin):
     )
 
 @admin.register(StockDetail)
-class StockDetailAdmin(admin.ModelAdmin):
+class StockDetailAdmin(ImportExportModelAdmin):
     
     def image_tag_1(self, obj):
         return format_html('<img src="{}" style="width:70px;"/>'.format(obj.img_url))
@@ -373,6 +448,7 @@ class StockDetailAdmin(admin.ModelAdmin):
         "custom_stock_four",
         "custom_stock_five",
     ]
+    resource_classes = [StockDetailResource]
 
 @admin.register(WMSShipData)
 class WMSShipDataAdmin(admin.ModelAdmin):
