5c42920984a218a3128fa4bd58d1000e357545d9
Oliver <olimovier@gmail.com>
2024-08-20 14:58:50 +0800

    开放 Schedules 接口

--- djangoapi/schedulers/urls.py
+++ djangoapi/schedulers/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path("send-dingtalk-msg/", views.schedule_send_dingtalk_msg),
-    # path("get-dashboard-data/", views.schedule_get_dashboard_data),
+    path("send-dingtalk-msg/", views.schedule_send_dingtalk_msg),
+    path("get-dashboard-data/", views.schedule_get_dashboard_data),
 ]
